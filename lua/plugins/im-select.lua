e4f477d632e791e19be4296b4724e2ab5e21596f
KazuyaMito <kazuya.mito@yappli.co.jp>
2024-12-09 17:06:27 +0900

    add im-select

---
+++ lua/plugins/im-select.lua
@@ -0,0 +1,9 @@
+return {
+    "keaising/im-select.nvim",
+    opts = {
+        default_command = "macism",
+        default_im_select = "com.google.inputmethod.Japanese.Roman",
+        set_default_events = { "VimEnter", "InsertEnter", "InsertLeave" },
+        set_previous_events = {},
+    }
+}
