eb00626473961239261b4481b22b97cc0cd674e8
KazuyaMito <kazuya.mito@yappli.co.jp>
2024-12-09 17:52:46 +0900

    remove im-select

--- lua/plugins/im-select.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "keaising/im-select.nvim",
-    opts = {
-        default_command = "macism",
-        default_im_select = "com.google.inputmethod.Japanese.Roman",
-        set_default_events = { "VimEnter", "InsertEnter", "InsertLeave" },
-        set_previous_events = {},
-    }
-}
